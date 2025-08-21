7b187b8744f5c37b634d254906665555e1fd63de
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-08-21 21:57:34 +0600

    fixed the responsive issue for products page

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -7,7 +7,7 @@ export default function ProductsPage() {
     <div className="max-w-7xl mx-auto p-6 my-10">
       <h1 className="text-3xl font-bold mb-6 text-center">Our Products</h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product) => (
           <div
             key={product.id}
